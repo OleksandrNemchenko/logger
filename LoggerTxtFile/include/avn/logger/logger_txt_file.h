bba0d334a49c09b3aea657b6fec071d964317ab6
Oleksandr Nemchenko <avn@nemchenko.net>
2019-10-25 11:54:05 +0300

    [txt] Automatic flush for selected levels

--- LoggerTxtFile/include/avn/logger/logger_txt_file.h
+++ LoggerTxtFile/include/avn/logger/logger_txt_file.h
@@ -17,26 +17,32 @@ public:
     using TBase = CLoggerTxtBase<LockThr, TChar>;
     using TString = std::basic_string<TChar>;
     using TStream = std::basic_ofstream<TChar>;
+    using TLevels = typename CLoggerBase<LockThr,TChar>::TLevels;
 
-    CLoggerTxtFile( bool local_time = true ): CLoggerTxtBase<LockThr, TChar>(local_time)    {}
+    CLoggerTxtFile( bool local_time = true ): CLoggerTxtBase<LockThr, TChar>(local_time), _flush_always( false )    {}
     CLoggerTxtFile( const std::filesystem::path &filename, std::ios_base::openmode mode = std::ios_base::out, bool local_time = true ) :
             CLoggerTxtFile( local_time )
     { OpenFile( filename, mode ); }
 
     CLoggerTxtFile& OpenFile( const std::filesystem::path &filename, std::ios_base::openmode mode = std::ios_base::out )    { _fstream.open( filename, mode ); return *this; }
     CLoggerTxtFile& CloseFile( const std::filesystem::path &filename, std::ios_base::openmode mode = std::ios_base::out )   { _fstream.close(); return *this; }
-    CLoggerTxtFile& FlushFile()                               { _fstream.flush(); return *this; }
-    CLoggerTxtFile& Imbue( const std::locale& loc )                 { _fstream.imbue( loc ); return *this; }
-    TStream& Stream()                                         { return _fstream; }
+    CLoggerTxtFile& FlushFile()                                 { _fstream.flush(); return *this; }
+    CLoggerTxtFile& SetFlushLevels( const TLevels& levels )     { _flush_levels = levels; _flush_always = false; return *this; }
+    CLoggerTxtFile& SetFlushLevels( TLevels &&levels )          { _flush_levels = std::move( levels ); _flush_always = false; return *this; }
+    CLoggerTxtFile& SetFlushAlways( bool flush_always = true )  { _flush_always = flush_always; return *this; }
+    CLoggerTxtFile& Imbue( const std::locale& loc )             { _fstream.imbue( loc ); return *this; }
+    TStream& Stream()                                           { return _fstream; }
 
-    bool IsOpenedFile() const                                 { return _fstream.is_open(); }
-    const TStream& Stream() const                             { return _fstream; }
+    bool IsOpenedFile() const                                   { return _fstream.is_open(); }
+    const TStream& Stream() const                               { return _fstream; }
 
 private:
 
     bool OutStrings( std::size_t level, std::chrono::system_clock::time_point time, TString&& data ) override final;
 
     TStream _fstream;
+    TLevels _flush_levels;
+    bool _flush_always;
 
 };
 
@@ -47,6 +53,9 @@ bool CLoggerTxtFile<LockThr, TChar>::OutStrings( std::size_t level, std::chrono:
 
     if( _fstream.is_open() ){
         _fstream << TBase::PrepareString( level, time, std::move(data) ) << std::endl;
+
+        if( _flush_always || _flush_levels.find(level) != _flush_levels.cend() )
+            _fstream.flush();
 
         return true;
     }
